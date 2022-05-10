6279b64e15d4084a7fc223b3e6d271545126bbea
Liis Normann <liis.normann@gmail.com>
2022-05-11 00:01:02 +0300

    Initial commit with navbar added

---
+++ header.php
@@ -0,0 +1,10 @@
+<div id="header">
+        <ul class="navbar">
+            <li class="nav-logo"><a href="index.php" class="logo">
+                    <i class="fa-solid fa-person-booth logo"></i>
+                </a>
+            </li>
+            <li class="title"><p>GET VOTING</p></li>
+            <li class="nav-admin"><a href="admin.php">Admin</a></li>
+        </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Make your pick</title>
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Fontawesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+<?php include 'header.php';?>
+<main class="container">
+
+</main>
+</body>
+</html>
+
